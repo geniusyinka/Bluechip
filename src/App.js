6677449fec8b788f220168f27e95db4f25dec469
yunggenius <yunggenius4@gmail.com>
2021-07-05 16:00:50 +0100

    responsive component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import Home from './components/Home';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import Navbar from './components/Nav/Navbar'
 
 function App() {
   return (
     <div className="App flex">
+      <div className="show-hide">
+        <Navbar/>
+      </div>
       <div id="sidebar" className=" w-1/4  ">
         <Sidebar/>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Search from './Search';
-import Category from './Category';
 import User from './User';
 import Usernav from './Usernav';
 import axios from 'axios';
@@ -26,25 +25,21 @@ function Home(props) {
     const [pictures, setPictures] = useState([])
     const [loading, setloading] = useState(true);
 
-    const url = 'https://api.unsplash.com/photos/random/?client_id=JY-4SM6RaS2gIUKelq3y0azFNJX2GA3mJ3DpH1n7qu0&orientation=portrait&count=30';
+    const url = 'https://api.unsplash.com/photos/random/?client_id=JY-4SM6RaS2gIUKelq3y0azFNJX2GA3mJ3DpH1n7qu0&orientation=portrait&count=28&query=man';
 
     function getPictures () {
         return fetch(url)
         .then(data => data.json())
-        //.then(json => console.log(json))
     }
 
-    // useEffect(() => {
-    //     getPictures()
-    //     .then(data => {
-    //         setPictures(data)
-    //         setloading(true)
-    //         console.log(pictures);
-    //     })
-
-        
-
-    // },[]);
+    useEffect(() => {
+        getPictures()
+        .then(data => {
+            setPictures(data)
+            setloading(false)
+            console.log(pictures);
+        })
+    },[]);
 
     const images = pictures.map((picture, index) => (
         <User 
@@ -52,28 +47,19 @@ function Home(props) {
     ))
     return (
         <div>
-            {/* <div className="home-search flex"> */}
             <div>
                 <Search/>
                 {/* <Usernav/> */}
             </div>
 
             
-                {/* {loading || !pictures ? 
+                {loading || !pictures ? 
                 <BeatLoader color={"#123abc"} loading={loading} css={override} size={50} speedMultiplier={1} margin={2} /> : 
                 <div className="user-comp mt-10  mb-10 flex flex-wrap justify-left">
                     {images}
-                    </div>
-                 } */}
-                 
-                 <div className="user-comp mt-10  mb-10 flex flex-wrap justify-left">
-
-                 <User 
-                     img='https://images.unsplash.com/photo-1557862921-37829c790f19?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNDQzMTB8MHwxfHNlYXJjaHwxfHxtYW58ZW58MHx8fHwxNjI1NDg1ODc5&ixlib=rb-1.2.1&q=80&w=1080' />
-                    </div>
-                
+                </div>
+                 }           
             
-
         </div>
     );
 }
